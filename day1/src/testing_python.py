330a364d7dcfef2871f6f2d74317e13f8d5ed2ae
Joakim Palmkvist <jpjocke@gmail.com>
2021-11-30 11:16:28 +0100

    read file utility2

---
+++ day1/src/testing_python.py
@@ -0,0 +1,20 @@
+from util.file_reader import FileReader
+
+print(__name__)
+
+
+fr = FileReader()
+
+print(fr.read("data/test1.txt"))
+
+data = fr.read_as_str_lines("data/test2.txt")
+
+for line in data:
+    print('line: ' + line)
+
+data2 = fr.read_as_int_lines("data/test3.txt")
+total = 0
+for num in data2:
+    print('num: ' + str(num))
+    total += num
+    print('total: ' + str(total))
--- day1/test/test_test.py
+++ day1/test/test_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from util.FileReader import FileReader
+from util.file_reader import FileReader
 
 
 class TestStringMethods(unittest.TestCase):
