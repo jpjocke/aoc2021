f501dc43acc6f0c5020c8664e3ff4f9411e4fa88
Joakim Palmkvist <jpjocke@gmail.com>
2021-11-30 14:51:01 +0100

    aoc 2020 test

--- day1/src/testing_python.py
+++ day1/src/testing_python.py
@@ -2,7 +2,6 @@ from util.file_reader import FileReader
 
 print(__name__)
 
-
 fr = FileReader()
 
 print(fr.read("data/test1.txt"))
@@ -12,9 +11,27 @@ data = fr.read_as_str_lines("data/test2.txt")
 for line in data:
     print('line: ' + line)
 
-data2 = fr.read_as_int_lines("data/test3.txt")
+data2 = fr.read_as_int_lines("data/2020_test1.txt")
 total = 0
 for num in data2:
     print('num: ' + str(num))
     total += num
     print('total: ' + str(total))
+
+data2.sort()
+for num in data2:
+    print('num: ' + str(num))
+
+# 2020 day 1 test
+for i, val_i in enumerate(data2):
+    print(str(i) + '-' + str(val_i))
+    for y, val_y in reversed(list(enumerate(data2))):
+        # print('--' + str(y) + '-' + str(valy))
+        if val_i + val_y == 2020:
+            print('FOUND: ' + str(val_i) + ' + ' + str(val_y))
+            break
+        if val_i + val_y < 2020:
+            # print('LESS')
+            break
+        if i == y:
+            break
